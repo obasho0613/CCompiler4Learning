43ce45d265c74cd9345560294630b1456876fd9b
sobata <obasho0613@gmail.com>
2025-01-29 00:10:29 +0900

    Added string literal.

--- 8cc.c
+++ 8cc.c
@@ -1,17 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
+
+#define BUFLEN 256
+
+void error(char *fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	vfprintf(stderr, fmt, args);
+	fprintf(stderr, "\n");
+	va_end(args);
+	exit(1);
+}
+
+void compile_number(int n) {
+	int c;
+	while ((c = getc(stdin)) != EOF) {
+		if (isspace(c))
+			// 空白なら数字の区切り
+			break;
+		if(!isdigit(c))
+			// 数字以外が混じっているのでNG
+			error("Invalid character in number: '%c'", c);
+		n = n * 10 + (c - '0');
+	}
+	printf(".text\n\t"
+                ".global intfn\n"
+                "intfn:\n\t"
+		"mov $%d, %%rax\n\t"
+		"ret\n", n);
+}
+void compile_string(void) {
+	char buf[BUFLEN];
+	int i = 0;
+	for (;;) {
+		int c = getc(stdin);
+		if (c == EOF)
+			// ダブルクォーテーションなしはエラー
+			error("Unterminated string");
+		if (c == '"')
+			// ダブルクォーテーション"は文字列の終わり
+			break;
+		if (c == '\\') {
+			c = getc(stdin);
+			if( c == EOF) error("Unterminated \\");
+		}
+		buf[i++] = c;
+		if (i == BUFLEN -1)
+			error("String too long");
+	}
+	buf[i] = '\0';
+	printf("\t.data\n\t"
+		".mydata:\n\t"
+		".string \"%s\"\n\t"
+		".text\n\t"
+		".global stringfn\n"
+		"stringfn:\n\t"
+		"lea .mydata(%%rip), %%rax\n\t"
+		"ret\n", buf);
+}
+
+void compile(void) {
+	int c = getc(stdin);
+	if (isdigit(c))
+		// 数字
+		return compile_number(c - '0');
+	if (c == '"')
+		// 文字列
+		return compile_string();
+	error("Don't know how to handle '%c'", c);
+}
 
 int main(int argc, char **argv) {
-	int val;
-	if (scanf("%d", &val) == EOF) {
-		perror("scanf");
-		exit(1);
-	}
-	printf("\t.text\n\t"
-		".global mymain\n"
-		"mymain:\n\t"
-		"mov $%d, %%eax\n\t"
-		"ret\n", val);
+	compile();
 	return 0;
 }
 
--- test.sh
+++ test.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+function compile {
+	echo "$1" | ./8cc > tmp.s
+	if [ $? -ne 0 ]; then
+		echo "Failed to compile $1"
+		exit
+	fi
+	gcc -o tmp.out driver.c tmp.s
+	if [ $? -ne 0 ]; then
+		echo "GCC failed"
+		exit
+	fi
+}
+
 function test {
 	expected="$1"
 	expr="$2"
 
-	echo "$expr" | ./8cc > tmp.s
-	if [ ! $? ]; then
-		echo "Failed to compile $expr"
-		exit
-	fi
-	gcc -o tmp.out driver.c tmp.s || exit
+	compile "$expr"
 	result="`./tmp.out`"
 	if [ "$result" != "$expected" ]; then
 		echo "Test failed: $expected expected but got $result"
@@ -17,11 +25,30 @@ function test {
 	fi
 }
 
+function testfail {
+	expr="$1"
+	echo "$expr" | ./8cc > /dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		echo "Should fail to compile, but succeded: $expr"
+		exit
+	fi
+}
+
+
 make -s 8cc
 
 test 0 0
 test 42 42
+test 102 102
+# "で囲まれたものを文字列として扱う"
+test abc '"abc"'
+test 0abc '"0abc"'
+test abc '"abc"'
 
-rm -f tmp.out tmp.s
+testfail '"abc'
+testfail '0abc'
+
+
+#rm -f tmp.out tmp.s
 echo "All tests passed"
 
